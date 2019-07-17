36b3400654660a39dc9d18b596e10c5bd47f6e58
Peter Martinson <peter.j.martinson@gmail.com>
2019-07-17 16:10:46 -0400

    Add the working files
    So far, this library performs the derivative of J (cost function,
    squared errors, whatever) and that's about it!

---
+++ Regress1D.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import numpy as np
+
+class Regress1D(object):
+    
+    def __init__(self):
+        self.X     = None # Feature array
+        self.y     = None # Response array
+        self.m     = None # Number of training examples
+        self.n     = None # Number of features
+        self.theta = None # Parameter array
+        self.dJ    = None # container for all current dJ/dTheta values
+        self.a     = 0.1  # Learning rate
+
+    def setNumberOfTrainingExamples(self, numpy_array):
+        self.m = len(numpy_array)
+
+    def incrementNumberOfFeatures(self):
+        if self.n is None:
+            self.n = 1
+        else:
+            self.n += 1
+
+    def addAnotherTheta(self):
+        if self.theta is None:
+            self.theta = np.array([1, 1])
+        else:
+            self.theta = np.append(self.theta, [1])
+
+    def setFeatures(self, numpy_array):
+        if not isinstance(numpy_array, np.ndarray):
+            raise TypeError('input must be a Numpy array')
+        if self.X is None:
+            self.setNumberOfTrainingExamples(numpy_array)
+            self.X = np.vstack((np.ones((1, self.m)), numpy_array))
+        else:
+            if len(numpy_array) == self.m:
+                self.X = np.vstack((self.X, numpy_array))
+            else:
+                raise TypeError('input array is not the right size')
+        self.incrementNumberOfFeatures()
+        self.addAnotherTheta()
+
+    def getFeatures(self):
+        return self.X
+
+    def setTargets(self, numpy_array):
+        if not isinstance(numpy_array, np.ndarray):
+            raise TypeError('input must be a Numpy array')
+        self.y = numpy_array
+
+    def getTarget(self):
+        return self.y
+
+    # np.sum(np.sum(theta*X.T, axis=1) - y)
+    # where X = [[1,1,1...],[x0_0, x0_1, x0_2, ...],[x1_0, x1_1, x1_2, ...],...]
+    def dJ_over_dTheta1(self):
+        result = 0
+        for i in range(0, self.m):
+            result += self.theta[0] + self.theta[1]*self.X[i] - self.y[i]
+        return result/self.m
+
+    def dJ_over_dTheta2(self):
+        result = 0
+        for i in range(0, self.m):
+            result += (self.theta[0] + self.theta[1]*self.X[i] - self.y[i]) * self.X[i]
+        return result/self.m
+
+    def evaluateDerivativeOfJ(self):
+        if self.X is None:
+            raise ValueError('X contains no data!')
+        if self.y is None:
+            raise ValueError('y contains no data!')
+        self.dJ = np.array([
+            (1/self.m) * np.sum( (np.sum(self.theta*self.X.T, axis=1) - self.y) ),
+            (1/self.m) * np.sum( (np.sum(self.theta*self.X.T, axis=1) - self.y) * self.X[1:])
+        ])
+
+    def evaluateNewThetas(self):
+        if self.dJ is None:
+            raise ValueError('X contains no data!')
+        if self.theta is None:
+            raise ValueError('y contains no data!')
+        if self.a is None:
+            raise ValueError('y contains no data!')
---
+++ test_regress1d.py
@@ -0,0 +1,160 @@
+import unittest
+import numpy as np
+import numpy.testing as npt
+from Regress1D import Regress1D
+
+class TestCanary(unittest.TestCase):
+    
+    def test_Tweets(self):
+        assert 1 == 1
+
+class Test_setFeatures(unittest.TestCase):
+
+    def setUp(self):
+        self.model = Regress1D()
+        self.numpy_array = np.array([1.,2.,3.])
+        self.m = 3
+
+    def test__Exists(self):
+        self.assertTrue(hasattr(self.model, 'setFeatures'))
+
+    def test__TakesNumpyArray(self):
+        not_an_array = "not a Numpy ndarray"
+        with self.assertRaises(TypeError):
+            self.model.setFeatures(not_an_array)
+
+    def test__Set_X_if_X_is_Empty(self):
+        correct_X = np.array([[1,1,1],[1.,2.,3.]])
+        self.model.setFeatures(self.numpy_array)
+        npt.assert_array_equal(self.model.X, correct_X)
+
+    def test__Set_m_if_X_is_Empty(self):
+        self.model.setFeatures(self.numpy_array)
+        assert self.m == self.model.m
+
+    def test__Throw_Exception_if_Array_is_Wrong_Size(self):
+        self.model.X = np.array([1.,1.,1.,1.])
+        self.model.m = 4
+        with self.assertRaises(TypeError):
+            self.model.setFeatures(self.numpy_array)
+
+    def test__Append_Array_if_X_is_Set(self):
+        self.model.X = np.array([[0.1,0.1,0.1],[1.,2.,3.]])
+        self.model.m = 3
+        correct_X = np.array([[0.1,0.1,0.1],[1.,2.,3.],[1.,2.,3.]])
+        self.model.setFeatures(self.numpy_array)
+        npt.assert_array_equal(self.model.X, correct_X)
+
+    def test__Set_n_equal_to_1_if_X_is_Empty(self):
+        self.model.setFeatures(self.numpy_array)
+        assert self.model.n == 1
+
+    def test__Increment_n_if_X_is_Set(self):
+        self.model.setFeatures(self.numpy_array)
+        self.model.setFeatures(self.numpy_array)
+        assert self.model.n == 2
+
+    def test__Set_theta_if_theta_is_not_yet_set(self):
+        correct_theta = np.array([1, 1])
+        self.model.setFeatures(self.numpy_array)
+        npt.assert_array_equal(self.model.theta, correct_theta)
+
+    def test__Add_another_theta_if_theta_is_already_set(self):
+        correct_theta = np.array([1,1,1,1])
+        self.model.setFeatures(self.numpy_array)
+        self.model.setFeatures(self.numpy_array)
+        self.model.setFeatures(self.numpy_array)
+        npt.assert_array_equal(self.model.theta, correct_theta)
+
+
+class Test_setTargets(unittest.TestCase):
+
+    def setUp(self):
+        self.model = Regress1D()
+        self.numpy_array = np.array([1,2,3])
+
+    def test__Exists(self):
+        self.assertTrue(hasattr(self.model, 'setTargets'))
+
+    def test__TakesNumpyArray(self):
+        not_an_array = "not a Numpy ndarray"
+        with self.assertRaises(TypeError):
+            self.model.setTargets(not_an_array)
+
+    def test__Sets_y(self):
+        self.model.setTargets(self.numpy_array)
+        npt.assert_array_equal(self.model.y, self.numpy_array)
+
+
+class Test_evaluateDerivativeOfJ(unittest.TestCase):
+
+    def setUp(self):
+        self.model = Regress1D()
+        self.feature_array = np.array([1,2,3,4,5,6,7,8,9,10])
+        self.target_array = self.feature_array * 1.7
+
+    def test__Exists(self):
+        self.assertTrue(hasattr(self.model, 'evaluateDerivativeOfJ'))
+
+    def test__Throws_If_X_Is_None(self):
+        with self.assertRaises(ValueError):
+            self.model.evaluateDerivativeOfJ()
+
+    def test__Throws_If_y_Is_None(self):
+        self.model.setFeatures(self.feature_array)
+        with self.assertRaises(ValueError):
+            self.model.evaluateDerivativeOfJ()
+
+    ## Need to figure out a test value for dJ, and the rest of the inputs
+    def test__Sets_dJ(self):
+        correct_dJ = [-2.85, -21.45]
+        self.model.setFeatures(self.feature_array)
+        self.model.setTargets(self.target_array)
+        self.model.evaluateDerivativeOfJ()
+        print("--- Diagnostic ---")
+        print("m =     ", self.model.m)
+        print("n =     ", self.model.n)
+        print("X =     ", self.model.X)
+        print("y =     ", self.model.y)
+        print("theta = ", self.model.theta)
+        print("dJ =    ", self.model.dJ)
+        npt.assert_array_equal(self.model.dJ, correct_dJ)
+
+class Test__evaluateNewThetas(unittest.TestCase):
+
+    def setUp(self):
+        self.model = Regress1D()
+        self.feature_array = np.array([1,2,3,4,5,6,7,8,9,10])
+        self.target_array = self.feature_array * 1.7
+
+    def test__Exists(self):
+        self.assertTrue(hasattr(self.model, 'evaluateNewThetas'))
+
+    def test__Throws_If_dJ_Is_None(self):
+        self.model.X = 1
+        self.model.y = 1
+        self.model.theta = 1
+        self.model.dJ = None
+        self.model.a = 1
+        with self.assertRaises(ValueError):
+            self.model.evaluateNewThetas()
+
+    def test__Throws_If_theta_Is_None(self):
+        self.model.X = 1
+        self.model.y = 1
+        self.model.theta = None
+        self.model.dJ = 1
+        self.model.a = 1
+        with self.assertRaises(ValueError):
+            self.model.evaluateNewThetas()
+
+    def test__Throws_If_a_Is_None(self):
+        self.model.X = 1
+        self.model.y = 1
+        self.model.theta = 1
+        self.model.dJ = 1
+        self.model.a = None
+        with self.assertRaises(ValueError):
+            self.model.evaluateNewThetas()
+
+
